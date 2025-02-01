4ab6aaf6995aab80e6ec64a7b97ccc3de8f88e04
Faris0520 <faris.6dsdiaf@gmail.com>
2025-02-01 17:12:45 +0700

    Refactor code to update User-Agent header in axios request for Instagram video download

--- index.js
+++ index.js
@@ -953,14 +953,10 @@ message.channel.send("https://cdn.discordapp.com/attachments/967061747011846244/
     if (!searchString) return message.channel.send("masukkan link");
     const url = `https://api.ryzendesu.vip/api/downloader/igdl?url=${searchString}`;
     const headers = {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
       'Accept-Language': 'en-US,en;q=0.9',
-      'Accept-Encoding': 'gzip, deflate, br',
       'Referer': 'https://www.google.com/',
-      'DNT': '1',
-      'Connection': 'keep-alive',
-      'Upgrade-Insecure-Requests': '1',
-      'Cache-Control': 'max-age=0',
     };
     let data = await axios.get(url, { headers }).then((res) => res.data);
 
