6e60ddcc86e40253634e5d8db7a834f9363f6df4
Faris Daffa <faris.6dsdiaf@gmail.com>
2025-02-13 13:34:11 +0700

    Refactor code to handle AI commands with OpenAI API

--- index.js
+++ index.js
@@ -1632,6 +1632,66 @@ message.channel.send("https://cdn.discordapp.com/attachments/967061747011846244/
       message.channel.send(responseText);
     } 
   }
+  if (command === "ai2") {
+    const OpenAI = require("openai");
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI
+    });
+  
+    if (!searchString)
+      return message.channel.send("Mohon berikan pertanyaan atau pesan untuk AI.");
+  
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "user", content: `${searchString}` }],
+      model: "o1",
+    });
+  
+    console.log(completion.choices[0]);
+    const responseText = completion.choices[0].message.content;
+  
+    // Memeriksa panjang pesan dan membaginya jika lebih dari 1999 karakter
+    if (responseText.length > 1999) {
+      // Membagi pesan menjadi potongan-potongan
+      let partLength = 1999;
+      for (let i = 0; i < responseText.length; i += partLength) {
+        const part = responseText.substring(i, i + partLength);
+        message.channel.send(part);
+      }
+    } else {
+      // Mengirim pesan jika kurang dari atau sama dengan 1999 karakter
+      message.channel.send(responseText);
+    } 
+  }
+  if (command === "ai3") {
+    const OpenAI = require("openai");
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI
+    });
+  
+    if (!searchString)
+      return message.channel.send("Mohon berikan pertanyaan atau pesan untuk AI.");
+  
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "user", content: `${searchString}` }],
+      model: "gpt-4o-realtime-preview",
+    });
+  
+    console.log(completion.choices[0]);
+    const responseText = completion.choices[0].message.content;
+  
+    // Memeriksa panjang pesan dan membaginya jika lebih dari 1999 karakter
+    if (responseText.length > 1999) {
+      // Membagi pesan menjadi potongan-potongan
+      let partLength = 1999;
+      for (let i = 0; i < responseText.length; i += partLength) {
+        const part = responseText.substring(i, i + partLength);
+        message.channel.send(part);
+      }
+    } else {
+      // Mengirim pesan jika kurang dari atau sama dengan 1999 karakter
+      message.channel.send(responseText);
+    } 
+  }
 if (command === "claude") {
   const got = (await import("got")).default;;
     if (!searchString)
